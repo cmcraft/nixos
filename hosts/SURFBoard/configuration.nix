7b842901713c225792fc3d447d25af658cd7db30
Constantine Craft <constantine.craft630@gmail.com>
2025-09-29 23:40:08 -0500

    moving impermanence to home-manager nixpkg

--- hosts/SURFBoard/configuration.nix
+++ hosts/SURFBoard/configuration.nix
@@ -159,6 +159,10 @@
   };
   
   home-manager = {
+    imports = [
+      impermanence.homeManagerModules.impermanence
+      ../../home/impermanence.nix # Your home-manager impermanence-configuration
+    ];
     extraSpecialArgs = { inherit inputs outputs; };
     users = {
       "cmcraft" = import ../../home/home.nix;
