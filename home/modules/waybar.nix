ba0e27393a21c9233c5a7a8cfe1c906addd47b34
Constantine Craft <constantine.craft630@gmail.com>
2025-10-02 12:50:12 -0500

    Filling out waybar config

--- home/modules/waybar.nix
+++ home/modules/waybar.nix
@@ -5,8 +5,104 @@
         layer = "top";
         position = "top";
         height = 30;
-        modules-left = [ ];
-        modules-center = [ ];
-        modules-right = [ "clock" ];
+        modules-left = [  
+          "custom/launcher"
+          "hyprland/workspaces"
+          "tray" 
+        ];
+        modules-center = [
+
+         ];
+        modules-right = [
+          "cpu"
+          "memory"
+          "disk"
+          "pulseaudio"
+          "network" 
+          "clock"
+        ];
+        clock = {
+      calendar = {
+        format = {
+          today = "<span color='#98971A'><b>{}</b></span>";
+        };
+      };
+      format = "  {:%H:%M}";
+      tooltip = "true";
+      tooltip-format = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+      format-alt = "  {:%d/%m}";
+    };
+    "hyprland/workspaces" = {
+      active-only = false;
+      disable-scroll = true;
+      format = "{icon}";
+      on-click = "activate";
+      format-icons = {
+        "1" = "I";
+        "2" = "II";
+        "3" = "III";
+        "4" = "IV";
+        "5" = "V";
+        "6" = "VI";
+        "7" = "VII";
+        "8" = "VIII";
+        "9" = "IX";
+        "10" = "X";
+        sort-by-number = true;
+      };
+      persistent-workspaces = {
+        "1" = [ ];
+        "2" = [ ];
+        "3" = [ ];
+        "4" = [ ];
+        "5" = [ ];
+      };
+    };
+    cpu = {
+      format = "<span foreground='${green}'> </span> {usage}%";
+      format-alt = "<span foreground='${green}'> </span> {avg_frequency} GHz";
+      interval = 2;
+      on-click-right = "hyprctl dispatch exec '[float; center; size 950 650] kitty --override font_size=14 --title float_kitty btop'";
+    };
+    memory = {
+      format = "<span foreground='${cyan}'>󰟜 </span>{}%";
+      format-alt = "<span foreground='${cyan}'>󰟜 </span>{used} GiB"; # 
+      interval = 2;
+      on-click-right = "hyprctl dispatch exec '[float; center; size 950 650] kitty --override font_size=14 --title float_kitty btop'";
+    };
+    disk = {
+      # path = "/";
+      format = "<span foreground='${orange}'>󰋊 </span>{percentage_used}%";
+      interval = 60;
+      on-click-right = "hyprctl dispatch exec '[float; center; size 950 650] kitty --override font_size=14 --title float_kitty btop'";
+    };
+    network = {
+      format-wifi = "<span foreground='${magenta}'> </span> {signalStrength}%";
+      format-ethernet = "<span foreground='${magenta}'>󰀂 </span>";
+      tooltip-format = "Connected to {essid} {ifname} via {gwaddr}";
+      format-linked = "{ifname} (No IP)";
+      format-disconnected = "<span foreground='${magenta}'>󰖪 </span>";
+    };
+    tray = {
+      icon-size = 20;
+      spacing = 8;
+    };
+    pulseaudio = {
+      format = "{icon} {volume}%";
+      format-muted = "<span foreground='${blue}'> </span> {volume}%";
+      format-icons = {
+        default = [ "<span foreground='${blue}'> </span>" ];
+      };
+      scroll-step = 2;
+      on-click = "pamixer -t";
+      on-click-right = "pavucontrol";
+    };
+    "custom/launcher" = {
+      format = "";
+      on-click = "random-wallpaper";
+      on-click-right = "rofi -show drun";
+      tooltip = "true";
+      tooltip-format = "Random Wallpaper";
+    };
         };
 }
