b8e3b83e107a92e4be7a8233743f69161afb18b9
Constantine Craft <constantine.craft630@gmail.com>
2025-10-02 13:02:28 -0500

    fonts!

--- home/packages.nix
+++ home/packages.nix
@@ -19,5 +19,15 @@
     hyprlock
     
     inputs.zen-browser.packages.${pkgs.system}.default
+
+    #stolen fonts
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.fira-code
+    nerd-fonts.caskaydia-cove
+    nerd-fonts.symbols-only
+    twemoji-color-font
+    noto-fonts-emoji
+    fantasque-sans-mono
+    maple-mono.truetype-autohint
   ];
 }
