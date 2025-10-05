f18c9c99064f0edafd2c7d746dc0fe575536cd24
Constantine Craft <constantine.craft630@gmail.com>
2025-10-05 16:35:38 -0500

    calendar died

--- home/modules/waybar/waybar.nix
+++ home/modules/waybar/waybar.nix
@@ -47,7 +47,6 @@ in
       calendar = {
         format = {
           today = "<span color='${purple}'><b>{}</b></span>";
-          weeks = "<span color='${cyan}'>{:%U}</span>";
           days = "<span color='${cyan}'></span>";
         };
       };
