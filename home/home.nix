23cc16be860f87f27df26b6ee64199d9b82f24e1
Constantine Craft <constantine.craft630@gmail.com>
2025-10-05 00:14:20 -0500

    vscode profile rename

--- home/home.nix
+++ home/home.nix
@@ -56,7 +56,7 @@ in
 
   programs.vscode = {
     enable = true;
-    extensions = with pkgs.vscode-extensions; [
+    profiles.default.extensions = with pkgs.vscode-extensions; [
       dracula-theme.theme-dracula
       vscodevim.vim
       yzhang.markdown-all-in-one
