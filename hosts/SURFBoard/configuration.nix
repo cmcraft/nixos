c0d6eefb399559146bee8a9076f6f5b9c8b807fe
Constantine Craft <constantine.craft630@gmail.com>
2025-10-05 12:42:09 -0500

    giving user users group

--- hosts/SURFBoard/configuration.nix
+++ hosts/SURFBoard/configuration.nix
@@ -156,7 +156,7 @@
     home = "/home/cmcraft";
     useDefaultShell = true;
     group = "cmcraft";
-    extraGroups = [ "wheel" "networkmanager" ];
+    extraGroups = [ "users" "wheel" "networkmanager" ];
     packages = with pkgs; [
       git
 
