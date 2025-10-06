a614dd4a50ccb873fb53e8e9c4662e49a5d29b71
Constantine Craft <constantine.craft630@gmail.com>
2025-10-05 19:41:52 -0500

    tweak mako

--- home/modules/mako/mako.nix
+++ home/modules/mako/mako.nix
@@ -2,5 +2,11 @@
 {
   services.mako = {
     enable = true;
+    settings = {
+      default-timeout = 5;
+      ignore-timeout = false;
+      icons = true;
+      border-radius = 5;
+    };
   };
 }
