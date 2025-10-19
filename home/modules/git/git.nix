47c9fc3d9cbeb497b610688f5cc89ebd2dfe1d9e
Constantine Craft <constantine.craft630@gmail.com>
2025-10-19 14:12:35 -0500

    changes to git config

--- home/modules/git/git.nix
+++ home/modules/git/git.nix
@@ -2,9 +2,11 @@
 {
   programs.git = {
     enable = true;
-    userName = "Constantine Craft";
-    userEmail = "constantine.craft630@gmail.com";
-    extraConfig = {
+    user = {
+      name = "Constantine Craft";
+      email = "constantine.craft630@gmail.com";
+    };
+    settings = {
       init.defaultBranch = "master";
       safe.directory = "/etc/nixos";
       # safe.directory = "/home/cmcraft/.dotfiles";
