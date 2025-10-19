4f85c2ccdcec4aa365a56b0e96715b80ae7110dc
Constantine Craft <constantine.craft630@gmail.com>
2025-10-18 23:43:01 -0500

    absolute path for default sops file?

--- modules/sops/sops.nix
+++ modules/sops/sops.nix
@@ -1,7 +1,7 @@
 { config, ... }:
 {
 
-  sops.defaultSopsFile = ./secrets/secrets.yaml;
+  sops.defaultSopsFile = /persist/etc/nixos/secrets/secrets.yaml;
   sops.defaultSopsFormat = "yaml";
 
   sops.age.keyFile = "/persist/home/cmcraft/.config/sops/age/secrets/keys.txt";
