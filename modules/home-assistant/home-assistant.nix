5125002ec4beeaefe66822f7bfb7e6b8d2ce1274
Constantine Craft <constantine.craft630@gmail.com>
2025-10-24 23:11:16 -0500

    home assist add mqtt things

--- modules/home-assistant/home-assistant.nix
+++ modules/home-assistant/home-assistant.nix
@@ -12,6 +12,13 @@
       # Recommended for fast zlib compression
       # https://www.home-assistant.io/integrations/isal
       "isal"
+      "mqtt"
+      "mqtt_eventstream"
+      "mqtt_json"
+      "mqtt_room"
+      "mqtt_statestream"
+      "myq"
+      "nest"
     ];
     extraPackages = python3Packages: with python3Packages; [
     # keep in case we need it later
