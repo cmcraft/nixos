4d439c580953b954bd215ec74b89ee477dc47aaf
Constantine Craft <constantine.craft630@gmail.com>
2025-10-28 21:14:44 -0500

    testing

--- modules/docker/docker.nix
+++ modules/docker/docker.nix
@@ -41,7 +41,6 @@
     redbot = {
       image = "phasecorex/red-discordbot:full";
       autoStart = false; # it doesn't like the token?
-      privileged = true;
       volumes = [ "/var/lib/containers/storage/redbot:/data" ];
       environmentFiles = [
         config.sops.templates."redbot".path
