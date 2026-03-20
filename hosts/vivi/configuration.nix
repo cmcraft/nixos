d641a701dc83dc8675f3176cd64d199df9c35898
Constantine Craft <cmcraft@users.noreply.github.com>
2026-03-19 21:31:22 -0500

    repack lmstudio for lms

--- hosts/vivi/configuration.nix
+++ hosts/vivi/configuration.nix
@@ -54,11 +54,36 @@
     ANTHROPIC_AUTH_TOKEN="lmstudio";
   };
 
-  #nixpkgs.config.packageOverrides = pkgs: {
-  #    llama-cpp = (
-  #      builtins.getFlake "github:ggerganov/llama.cpp"
-  #    ).packages.${builtins.currentSystem}.default;
-  #  };
+  nixpkgs.config.packageOverrides = pkgs: {
+    lmstudio = (
+      let
+        original = pkgs.lmstudio;
+        wrapper = pkgs.writeShellScriptBin "lms" ''
+          exec ${original}/bin/lm-studio "$@"
+        '';
+      in
+      (pkgs.stdenv.mkDerivation {
+        name = "lmstudio-fixed";
+        nativeBuildInputs = [ pkgs.makeWrapper ];
+        buildCommand =
+          let
+            bin = original + "/bin";
+            wrapperBin = wrapper + "/bin";
+          in
+          ''
+            mkdir -p $out/bin
+            cp ${bin}/lm-studio $out/bin/
+            ln -s lm-studio $out/bin/lms
+            cat > $out/bin/lms-wrapper <<EOF
+              #!/bin/sh
+              # This is a wrapper to ensure lms uses the bwrap environment
+              exec ${wrapperBin}/lms "$@"
+            EOF
+            chmod +x $out/bin/lms-wrapper
+          '';
+      })
+    );
+  };
   environment.systemPackages = [
     pkgs.llama-cpp
     pkgs.llamacpp-rocm
@@ -71,6 +96,7 @@
     inputs.home-manager.packages.${pkgs.stdenv.hostPlatform.system}.default
     pkgs.base16-schemes
     pkgs.unzip
+    pkgs.patchelf
 
     # LM Studio CLI
     pkgs.lmstudio
