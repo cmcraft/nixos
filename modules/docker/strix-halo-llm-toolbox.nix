8dd27883aa6c933b7c7ebe7a5dafe6fd1b3676c2
Constantine Craft <cmcraft@users.noreply.github.com>
2026-03-25 17:48:59 -0500

    adding open-webui and comfyui containers

--- modules/docker/strix-halo-llm-toolbox.nix
+++ modules/docker/strix-halo-llm-toolbox.nix
@@ -9,7 +9,7 @@
 
   virtualisation.oci-containers.containers.strix-halo-comfyui = {
     image = "docker.io/kyuz0/amd-strix-halo-comfyui:latest";
-    autoStart = false;
+    autoStart = true;
     devices = [ "/dev/dri" "/dev/kfd" ];
     extraOptions = [
       "--group-add=video"
@@ -19,6 +19,17 @@
     volumes = [
       "/var/lib/containers/storage/strix-halo/comfyui:/home/cmcraft"
     ];
-    ports = [ "8080:8188" ];  # ComfyUI default port
+    ports = [ "8188:8188" ];  # ComfyUI default port
+  };
+
+  # Open-webui
+
+  virtualisation.oci-containers.containers.open-webui = {
+    image = "ghcr.io/open-webui/open-webui:main";
+    autoStart = true;
+    volumes = [
+      "open-webui:/app/backend/data"
+    ];
+    ports = [ "3000:8080" ];  # ComfyUI default port
   };
 }
