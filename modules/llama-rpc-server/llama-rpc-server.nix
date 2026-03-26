dee2ed5f71a55e3467fa3bfde9d12c153482fbe2
Constantine Craft <cmcraft@users.noreply.github.com>
2026-03-25 22:49:40 -0500

    see what sticks

--- modules/llama-rpc-server/llama-rpc-server.nix
+++ modules/llama-rpc-server/llama-rpc-server.nix
@@ -3,12 +3,5 @@
   services.llamacpp-rpc-servers.main = {
     enable = true;
     package = pkgs.llamacpp-rocm;  # Choose your GPU target
-    threads = 32;
-    host = "127.0.0.1";  # Listen on all interfaces
-    port = 50052;
-    memory = 8192;  # 8GB backend memory
-    device = "0";  # GPU device ID
-    enableCache = true;
-    openFirewall = true;
   };
 }
