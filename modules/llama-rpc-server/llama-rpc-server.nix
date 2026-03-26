58457393b2aa66c206628ad7d93f19bfb75ad9fb
Constantine Craft <cmcraft@users.noreply.github.com>
2026-03-25 22:46:40 -0500

    idk

--- modules/llama-rpc-server/llama-rpc-server.nix
+++ modules/llama-rpc-server/llama-rpc-server.nix
@@ -1,10 +1,10 @@
 { pkgs, ... }: 
 {
-  services.llamacpp-rpc-server = {
+  services.llamacpp-rpc-servers.main = {
     enable = true;
     package = pkgs.llamacpp-rocm;  # Choose your GPU target
     threads = 32;
-    host = "0.0.0.0";  # Listen on all interfaces
+    host = "127.0.0.1";  # Listen on all interfaces
     port = 50052;
     memory = 8192;  # 8GB backend memory
     device = "0";  # GPU device ID
