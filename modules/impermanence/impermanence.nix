01a5f33ca84364a0d5c7f8f3202b5639010119de
Constantine Craft <cmcraft@users.noreply.github.com>
2026-03-25 22:12:31 -0500

    idk

--- modules/impermanence/impermanence.nix
+++ modules/impermanence/impermanence.nix
@@ -25,7 +25,7 @@
       "/var/lib/terraria"
       "/var/lib/zigbee2mqtt"
       { directory = "/var/lib/colord"; user = "colord"; group = "colord"; mode = "u=rwx,g=rx,o="; }
-      { directory = "/var/lib/open-webui"; }
+      { directory = "/var/lib/open-webui"; mode = "0700"; }
       # { directory = "/var/lib/factorio"; user = "factorio"; group = "factorio"; mode = "u=rwx,g=rx,o="; }
     ];
 
