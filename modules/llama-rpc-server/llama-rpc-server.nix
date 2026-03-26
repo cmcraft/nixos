ded3d4efb57591c180eb30813a82aa509afac4fb
Constantine Craft <cmcraft@users.noreply.github.com>
2026-03-25 22:33:53 -0500

    testing

--- modules/llama-rpc-server/llama-rpc-server.nix
+++ modules/llama-rpc-server/llama-rpc-server.nix
@@ -7,7 +7,7 @@
     host = "0.0.0.0";  # Listen on all interfaces
     port = 50052;
     memory = 8192;  # 8GB backend memory
-    device = "0";  # GPU device ID
+    # device = "0";  # GPU device ID
     enableCache = true;
     openFirewall = true;
   };
