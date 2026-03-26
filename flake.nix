ed2427658db92b1a5fc7197eacefc4b78f60d41b
Constantine Craft <cmcraft@users.noreply.github.com>
2026-03-25 19:01:02 -0500

    rpc service later

--- flake.nix
+++ flake.nix
@@ -144,21 +144,7 @@
         })
 
         llamacpp-rocm.nixosModules.rpc-server
-
-        ({ pkgs, ... }: {
-          services.llamacpp-rpc-servers = {
-            enable = true;
-            package = pkgs.llamacpp-rocm;  # Choose your GPU target
-            threads = 32;
-            host = "0.0.0.0";  # Listen on all interfaces
-            port = 50052;
-            memory = 8192;  # 8GB backend memory
-            device = "0";  # GPU device ID
-            enableCache = true;
-            openFirewall = true;
-          };
-        })
-
+        
         {
             imports = [ home-manager.nixosModules.home-manager ];
             home-manager.users.cmcraft =
