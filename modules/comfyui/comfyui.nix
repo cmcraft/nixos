f75348c3e433c87627dee5c98a05354212949a4f
Constantine Craft <cmcraft@users.noreply.github.com>
2026-03-26 20:06:34 -0500

    per google

--- modules/comfyui/comfyui.nix
+++ modules/comfyui/comfyui.nix
@@ -8,12 +8,16 @@
     listenAddress = "0.0.0.0";  # Use "0.0.0.0" for network access
     dataDir = "/var/lib/comfyui";
     openFirewall = true;
-    extraArgs = [ "--enable-dynamic-vram" ];
+    extraArgs = [ 
+      "--enable-dynamic-vram"
+      "--disable-pinned-memory" 
+    ];
     environment = {
-      HSA_OVERRIDE_GFX_VERSION = "11.5.0"; 
+      HSA_OVERRIDE_GFX_VERSION = "11.5.0";
+      PYTORCH_HIP_ALLOC_CONF = "garbage_collection_threshold:0.6,max_split_size_mb:512"; 
       HIP_VISIBLE_DEVICES = "0";
       # Point to the system-wide ROCm path
-      ROCM_PATH = "/run/current-system/sw";
+      ROCM_PATH = "${pkgs.rocmPackages.clr}";
       ROC_ENABLE_PRE_ALLOCATION = "1";
       PYTORCH_ROCM_ARCH = "gfx1151"; 
       LD_LIBRARY_PATH = "${pkgs.rocmPackages.clr}/lib:${pkgs.libdrm}/lib"; 
