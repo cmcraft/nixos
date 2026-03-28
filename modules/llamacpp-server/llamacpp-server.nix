3a9554a55c34839ff8167ac9445459102b46c0e1
Constantine Craft <cmcraft@users.noreply.github.com>
2026-03-28 01:06:34 -0500

    try --mmproj-load-on-demand

--- modules/llamacpp-server/llamacpp-server.nix
+++ modules/llamacpp-server/llamacpp-server.nix
@@ -9,6 +9,7 @@
       ExecStart = ''
         ${inputs.llamacpp-rocm.packages.${pkgs.stdenv.hostPlatform.system}.llamacpp-rocm}/bin/llama-server \
           --models-dir /home/cmcraft/.cache/llama.cpp \
+          --mmproj-load-on-demand \
           --sleep-idle-seconds 900 \
           --models-max 4 \
           --host 127.0.0.1 \
