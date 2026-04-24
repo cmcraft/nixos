99f39bb0edafcebd8d0c0de2005215f43e9a4c96
Constantine Craft <cmcraft@users.noreply.github.com>
2026-04-23 21:14:29 -0500

    per google

--- modules/sillytavern/sillytavern.nix
+++ modules/sillytavern/sillytavern.nix
@@ -9,6 +9,22 @@
 
   };
 
+  systemd.services.sillytavern = {
+    serviceConfig = {
+      ExecStartPre = [
+        "+${config.pkgs.coreutils}/bin/mkdir -p /var/lib/SillyTavern"
+        "+${config.pkgs.coreutils}/bin/cp -f ${config.sops.templates."sillytavern-config".path} /var/lib/SillyTavern/config.yaml"
+        "+${config.pkgs.coreutils}/bin/chown sillytavern:sillytavern /var/lib/SillyTavern/config.yaml"
+        "+${config.pkgs.coreutils}/bin/chmod 0600 /var/lib/SillyTavern/config.yaml"
+      ];
+
+      ExecStart = [
+        "" # empty old command
+        "${config.services.sillytavern.package}/bin/sillytavern --configPath /var/lib/SillyTavern/config.yaml"
+      ];
+    };
+  };
+
   networking.firewall = {
     enable = true;
     allowedTCPPorts = [ 8045 ];
