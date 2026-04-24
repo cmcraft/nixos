3ca29d5a34f469de99410b575786d1210a41d223
Constantine Craft <cmcraft@users.noreply.github.com>
2026-04-23 20:37:21 -0500

    reading is hard

--- modules/sillytavern/sillytavern.nix
+++ modules/sillytavern/sillytavern.nix
@@ -6,7 +6,11 @@
     port = 8045;
     listen = true;
     whitelist = false;
+  };
+
+  systemd.services.sillytavern = {
     serviceConfig = {
+      # This reads the generated environment file at service startup
       EnvironmentFile = config.sops.templates."sillytavern-env".path;
     };
   };
