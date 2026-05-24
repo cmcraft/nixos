7e96acc07eac3b150b59b0a2d3f79be94d70d9d8
Constantine Craft <cmcraft@users.noreply.github.com>
2026-05-24 11:24:15 -0500

    make hermes better?

--- modules/hermes/hermes-agent.nix
+++ modules/hermes/hermes-agent.nix
@@ -2,8 +2,36 @@
 { config, ... }: {
   services.hermes-agent = {
     enable = true;
+
+    restart = "on-failure";
+    restartSec = 5;
+
     settings.model.base_url = "http://127.0.0.1:13305/v1";
     environmentFiles = [ config.sops.secrets."hermes-env".path ];
     addToSystemPackages = true;
+
+    # base.nix
+    settings = {
+      # model.default = "";
+      toolsets = [ "all" ];
+      max_turns = 100;
+      terminal = { backend = "local"; cwd = "."; timeout = 180; };
+      compression = {
+        enabled = true;
+        threshold = 0.85;
+        # summary_model = "";
+      };
+    };
+
+    # personality.nix
+    settings = {
+      display = { compact = false; personality = "kawaii"; };
+      memory = { memory_enabled = true; user_profile_enabled = true; };
+      agent = { max_turns = 60; verbose = false; };
+    };
+
+    documents = {
+      "USER.md" = ./documents/USER.md;
+    };
   };
 }
