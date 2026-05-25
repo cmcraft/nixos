c28dd84b6e62285125f43fbfacbc415417776f39
Constantine Craft <cmcraft@users.noreply.github.com>
2026-05-24 23:17:06 -0500

    add messaging to telegram

--- modules/hermes/hermes-agent.nix
+++ modules/hermes/hermes-agent.nix
@@ -8,6 +8,7 @@
 
     environmentFiles = [ config.sops.secrets."hermes-env".path ];
     addToSystemPackages = true;
+    extraDependencyGroups = [ "messaging" ];
 
     # base.nix
     settings = {
