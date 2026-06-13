aa9111909709e5d26f886f1d93207bc12f7c0158
Constantine Craft <cmcraft@users.noreply.github.com>
2026-06-13 00:46:46 -0500

    Tailscale needs a persistent dir

--- modules/tailscale/tailscale.nix
+++ modules/tailscale/tailscale.nix
@@ -37,4 +37,11 @@
 
   networking.nameservers = [ "100.100.100.100" "8.8.8.8" "1.1.1.1" ];
   networking.search = [ "example.ts.net" ];
+
+  environment.persistence."/persist" = {
+    hideMounts = true;
+    directories = [
+      "/var/lib/tailscale"
+    ];
+  };
 }
