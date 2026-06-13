5ab549ef85bc118580f0855d9039d23cdc95fdaf
Constantine Craft <cmcraft@users.noreply.github.com>
2026-06-12 22:40:56 -0500

    Lemonade Tweaks

--- modules/containers/lemonade.nix
+++ modules/containers/lemonade.nix
@@ -24,22 +24,16 @@
       "GGML_VULKAN_DEVICE" = "0";
       "GGML_VK_PREFER_HOST_MEMORY" = "1";
       "RADV_PERFTEST" = "nogttspill";
-      
-      # KV Cache Quantization (Huge bandwidth savings)
-      "LEMONADE_KV_CACHE_TYPE" = "q4_0"; # Reduces KV cache size/bandwidth by 4x
+      "HSA_OVERRIDE_GFX_VERSION" = "11.5.0";
+
+      "LEMONADE_KV_CACHE_TYPE" = "q8_0";
       "LEMONADE_CTX_CACHE_SIZE" = "5";
       
-      # Speculative Decoding (The "Draft" model speedup)
-      # Ensure this file exists in your models volume!
-      # "LEMONADE_SPECULATIVE_MODEL" = "/root/.cache/huggingface/hub/models--Qwen--Qwen2.5-1.5B-Instruct-GGUF/snapshots/91cad51170dc346986eccefdc2dd33a9da36ead9/qwen2.5-1.5b-instruct-q8_0.gguf";
-      "LEMONADE_DRAFT_MODEL" = "/root/.cache/huggingface/hub/models--unsloth--Qwen3.5-4B-MTP-GGUF/snapshots/86835bf9949e4d14d6860f7910b1340ad4f271a9/Qwen3.5-4B-UD-Q4_K_XL.gguf";
-      "LEMONADE_DRAFT_NGL" = "-1";  # offload to GPU
-
       # --- MEMORY MANAGEMENT ---
       "LEMONADE_CTX_SIZE" = "98304"; 
       "LEMONADE_MAX_LOADED_MODELS" = "7"; 
       "LEMONADE_GLOBAL_TIMEOUT" = "0"; 
-      "LEMONADE_MAX_VRAM_GB" = "96";
+      "LEMONADE_MAX_VRAM_GB" = "124";
     };
 
     volumes = [
@@ -49,7 +43,6 @@
     ];
 
     extraOptions = [
-      # "--shm-size=96gb" 
       "--cap-add=SYS_PTRACE"
       "--security-opt=seccomp=unconfined"
       "--ipc=host" # Better memory throughput between host and container
